3e26df0a6ed9a6030429360439f9d9f39d5172c6
xdpranali <143471649+xdpranali@users.noreply.github.com>
2024-11-15 02:16:31 -0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -12,6 +12,13 @@ app.use(express.json());
 //mounting api routes
 app.use("/api/v1",router)
 
+app.get("/", (req, res)=>{
+  res.json({
+    success: true,
+    message: "Server Running Successfully"
+  })
+})
+
 dotenv.config();
 
 dbConnect();
